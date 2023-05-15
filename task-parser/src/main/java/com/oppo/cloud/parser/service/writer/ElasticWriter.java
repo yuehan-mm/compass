f24c4beb847050481d4706a52ceff250d286fcd2
yuehan-mm <827890027@qq.com>
2023-05-15 11:17:51 +0800

    修改redis配置

--- task-parser/src/main/java/com/oppo/cloud/parser/service/writer/ElasticWriter.java
+++ task-parser/src/main/java/com/oppo/cloud/parser/service/writer/ElasticWriter.java
@@ -189,7 +189,6 @@ public class ElasticWriter {
         String index = detectorPrefix + DateUtil.formatToDay(detectionStorage.getExecutionTime());
         UpdateApi api = new UpdateApi();
         try {
-            log.info("saveDetectorStorage:{}", detectionStorage.toString());
             UpdateResponse resp = api.upsertJson(client, index, UUID.randomUUID().toString(), JSON.toJSONString(detectionStorage));
             log.info("saveDetectorStorage:{},{}", detectionStorage.getApplicationId(), resp);
         } catch (Exception e) {
